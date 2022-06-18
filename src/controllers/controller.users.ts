bb9466d5ec09224d1597f0f20ce1834ba203281b
restuwahyu13 <restuwahyu13@gmail.com>
2022-06-19 01:32:30 +0700

    publish files

--- src/controllers/controller.users.ts
+++ src/controllers/controller.users.ts
@@ -13,9 +13,9 @@ export class UsersController {
     return async (req: Request, res: Response, next: NextFunction): Promise<OutgoingMessage> => {
       try {
         const response: APIResponse = await this.service.getAllUsers()
-        return res.status(200).json(response)
+        return res.status(response.stat_code).json(response)
       } catch (e: any) {
-        return res.status(400).json(e)
+        return res.status(e.stat_code).json(e)
       }
     }
   }
