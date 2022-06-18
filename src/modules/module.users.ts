f65f63e03978d4305c3cf4b066441516e2693b84
restuwahyu13 <restuwahyu13@gmail.com>
2022-06-18 16:30:46 +0700

    publish files

--- src/modules/module.users.ts
+++ src/modules/module.users.ts
@@ -1,14 +1,12 @@
-import { singleton as Singleton } from 'tsyringe'
-
 import { UsersService } from '@services/service.users'
 import { UsersController } from '@controllers/controller.users'
 import { UsersRoute } from '@routes/route.users'
-import { Registry, Delay } from '@libs/lib.di'
+import { Registry, Delay, Injectable } from '@libs/lib.di'
 
 @Registry([
   { token: 'UsersService', useClass: Delay(() => UsersService) },
   { token: 'UsersController', useClass: Delay(() => UsersController) },
   { token: 'UsersRoute', useClass: Delay(() => UsersRoute) }
 ])
-@Singleton()
+@Injectable()
 export class UsersModule extends UsersRoute {}
