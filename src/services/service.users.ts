0c0330ced26ae49a7345ee956c9fef6fe32ff5bc
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2022-06-28 12:39:36 +0700

    Update service.users.ts

--- src/services/service.users.ts
+++ src/services/service.users.ts
@@ -2,13 +2,13 @@ import { StatusCodes as status } from 'http-status-codes'
 
 import { Users } from '@entities/entitie.users'
 import { Inject, Service, Repository } from '@helpers/helper.di'
-import { apiResponse } from '@helpers/helper.apiResponse'
+import { apiResponse, APIResponse } from '@helpers/helper.apiResponse'
 
 @Service()
 export class UsersService {
   constructor(@Inject('UsersModel') private model: Repository<Users>) {}
 
-  async getAllUsers(): Promise<any> {
+  async getAllUsers(): Promise<APIResponse> {
     try {
       const getAllUsers: Users[] = await this.model.find({})
       return Promise.resolve(apiResponse(status.OK, 'Users already to use', getAllUsers, null))
